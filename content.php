ef8c07fdd7da71b2e458a2ecf175ececa91d237a
Unknown <chouania.mehdi@hotmail.fr>
2018-11-21 15:26:45 +0100

    Modif page accueil

--- content.php
+++ content.php
@@ -1,64 +1,359 @@
 <!-- Page Content -->
-<div class="container">
+<section id="home-banner">
+	<div class="container-fluid">
+		<div class="slider-wrap">
+			<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+				<!-- Indicators -->
+				<ol class="carousel-indicators">
+					<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+				</ol>
+			  <!-- Wrapper for slides -->
+				<div class="carousel-inner" role="listbox">
+					<div class="item active">
+						<div class="slide-1">
+							<img src="http://via.placeholder.com/1598x715" class="img-responsive"/>
+						</div>
+						<div class="text-wrap ">
+							<h2>Discover The Best</h2>
+							<h1>Activities & Events</h1>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lobortis pretium enim, ut accumsan ligula faucibus eget...</p>
+							<div class="button-list">
+								<a href="" class="banner-button btn-blue hidden-xs">Register Now</a>
+								<a href="" class="banner-button btn-white">Read More</a>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="slide-1">
+							<img src="http://via.placeholder.com/1598x715" class="img-responsive"/>
+						</div>
+						<div class="text-wrap">
+							<h2>Discover The Best</h2>
+							<h1>Activities & Events</h1>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lobortis pretium enim, ut accumsan ligula faucibus eget...</p>
+							<div class="button-list">
+								<a href="" class="banner-button btn-blue hidden-xs">Register Now</a>
+								<a href="" class="banner-button btn-white">Read More</a>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="slide-1">
+							<img src="http://via.placeholder.com/1598x715" class="img-responsive"/>
+						</div>
+						<div class="text-wrap">
+							<h2>Discover The Best</h2>
+							<h1>Activities & Events</h1>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lobortis pretium enim, ut accumsan ligula faucibus eget...</p>
+							<div class="button-list">
+								<a href="" class="banner-button btn-blue hidden-xs">Register Now</a>
+								<a href="" class="banner-button btn-white">Read More</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- Controls -->
+				<a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+					<span class="fa fa-angle-left"></span>
+				</a>
+				<a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+					<span class="fa fa-angle-right"></span>
+				</a>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="men-women">
+	<div class="container-fluid pl-0 pr-0">
+		<div class="row mr-0 ml-0" >
+			<div class="col-sm-4 pl-0 pr-0">
+				<div class="men-section small text-center">
+					<h2>For Men</h2>
+					<p>Lorem ipsum dolor et..</p>
+					<a class="btn-white">Read More</a>
+				</div>
+			</div>
+			<div class="col-sm-4 pl-0 pr-0">
+				<div class="men-section main text-center">
+					<h2>Sed ut perspiciatis unde omnis iste natus error sit</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+					<a class="btn-blue">Read More</a>
+				</div>
+			</div>
 
-    <div class="row">
+			<div class="col-sm-4 pl-0 pr-0">
+				<div class="men-section small text-center">
+					<h2>For Women</h2>
+					<p>Lorem ipsum dolor et..</p>
+					<a class="btn-white">Read More</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="services">
+	<div class="container">
+		<div class="section-heading text-center">
+			<h1>Our Services</h1>
+		</div>
+		<div class="service-list">
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="service-left text-right">
+						<div class="service top">
+							<div class="icon-wrap">
+								<i class="fa fa-free-code-camp" aria-hidden="true"></i>
+							</div>
+							<a href="">SPARKLING CLEAN</a>
+							<p>We keep your home sparkling clean and germ free. Our disinfecting process kills 99% of common bacteria and viruses</p>
+						</div>
+						<div class="service bottom mid">
+							<div class="icon-wrap">
+								<i class="fa fa-free-code-camp" aria-hidden="true"></i>
+							</div>
+							<a href="">SPARKLING CLEAN</a>
+							<p>We keep your home sparkling clean and germ free. Our disinfecting process kills 99% of common bacteria and viruses</p>
+						</div>
+						<div class="service bottom">
+							<div class="icon-wrap">
+								<i class="fa fa-free-code-camp" aria-hidden="true"></i>
+							</div>
+							<a href="">SPARKLING CLEAN</a>
+							<p>We keep your home sparkling clean and germ free. Our disinfecting process kills 99% of common bacteria and viruses</p>
+						</div>
 
-        <!-- Blog Entries Column -->
-        <div class="col-md-8">
-            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-              <!-- Indicators -->
-              <ol class="carousel-indicators">
-                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-              </ol>
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<div class="image-wrap">
+						<img src="http://www.cybersense.co.nz/images/cybersense.co.nz/for-men-coral-deep-blue-male-fleece-autumn-and-winter-long-section-embossed-sleepwear-robes-51VV.jpg" class="img-responsive">
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<div class="service-right text-left">
+						<div class="service top">
+							<div class="icon-wrap">
+								<i class="fa fa-free-code-camp" aria-hidden="true"></i>
+							</div>
+							<a href="">SPARKLING CLEAN</a>
+							<p>We keep your home sparkling clean and germ free. Our disinfecting process kills 99% of common bacteria and viruses</p>
+						</div>
+						<div class="service bottom mid">
+							<div class="icon-wrap">
+								<i class="fa fa-free-code-camp" aria-hidden="true"></i>
+							</div>
+							<a href="">SPARKLING CLEAN</a>
+							<p>We keep your home sparkling clean and germ free. Our disinfecting process kills 99% of common bacteria and viruses</p>
+						</div>
+						<div class="service bottom">
+							<div class="icon-wrap">
+								<i class="fa fa-free-code-camp" aria-hidden="true"></i>
+							</div>
+							<a href="">SPARKLING CLEAN</a>
+							<p>We keep your home sparkling clean and germ free. Our disinfecting process kills 99% of common bacteria and viruses</p>
+						</div>
+					</div>
+				</div>
 
-              <!-- Wrapper for slides -->
-              <div class="carousel-inner" role="listbox">
-                <div class="item active">
-                    <img src="images/sciencesu.png" width="650" height="366" style= margin:auto>
-                </div>
-                <div class="item">
-                  <img src="images/sciencesu2.png" width="650" height="366" style= margin:auto >
-                </div>
-              </div>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="product-list">
+	<div class="container">
+		<div class="section-heading text-center">
+			<h1>Our Products</h1>
+		</div>
+		<div class="row product-list">
+			<div class="col-sm-3">
+				<div class="productw-wrap">
+					<div class="product-image">
+						<img src="https://4.imimg.com/data4/AR/NB/MY-4289718/men-s-casual-shirt-500x500.png" class="img-responsive">
+					</div>
+					<div class="prduct-text">
+						<a href="" class="name">Lorem Ipsum dolor sit...</a>
+						<ins class="price">$123.00</ins>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-3">
+				<div class="productw-wrap">
+					<div class="product-image">
+						<img src="https://4.imimg.com/data4/AR/NB/MY-4289718/men-s-casual-shirt-500x500.png" class="img-responsive">
+					</div>
+					<div class="prduct-text">
+						<a href="" class="name">Lorem Ipsum dolor sit...</a>
+						<ins class="price">$123.00</ins>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-3">
+				<div class="productw-wrap">
+					<div class="product-image">
+						<img src="https://4.imimg.com/data4/AR/NB/MY-4289718/men-s-casual-shirt-500x500.png" class="img-responsive">
+					</div>
+					<div class="prduct-text">
+						<a href="" class="name">Lorem Ipsum dolor sit...</a>
+						<ins class="price">$123.00</ins>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-3">
+				<div class="productw-wrap">
+					<div class="product-image">
+						<img src="https://4.imimg.com/data4/AR/NB/MY-4289718/men-s-casual-shirt-500x500.png" class="img-responsive">
+					</div>
+					<div class="prduct-text">
+						<a href="" class="name">Lorem Ipsum dolor sit...</a>
+						<ins class="price">$123.00</ins>
+					</div>
+				</div>
+			</div>
 
-              <!-- Controls -->
-              <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                <span class="sr-only">Précédent</span>
-              </a>
-              <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                <span class="sr-only">Suivant</span>
-              </a>
-            </div>
-            <hr>
-            <?php
-               $articles = bdd_article();
-                while($article = $articles->fetch()){
-                ?>
-                    <h2>
-                        <a href="artsolo.php?id=<?php echo $article['id']; ?>">
-                           <?php echo $article['titre']; ?>
-                        </a>
-                    </h2>
-                    <p class="lead">
-                    by <a href="artsolo.php?id=
-                        <?php echo $article['id']; ?>">
-                        <?php echo $article['auteur']; ?>
-                        </a>
-                    </p>
-                    <p><span class="glyphicon glyphicon-time"></span> Posté le <?php echo $article['date'];?></p>
-                    <hr>
-                    <img class="img-responsive" src="/images/<?php echo $article['image'];?>"/>
-                    <hr>
-                    <p><?php echo $article['contenu'];?></p>
-                <hr>
-                <?php }
-                ?>
-        </div>
-    </div>
+		</div>
+	</div>
+</section>
+<section>
+  <div class="row">
 
-    <!-- /.row -->
-    <hr>
-</div>
+          <hr>
+          <?php
+             $articles = bdd_article();
+              while($article = $articles->fetch()){
+              ?>
+                  <h2>
+                      <a href="artsolo.php?id=<?php echo $article['id']; ?>">
+                         <?php echo $article['titre']; ?>
+                      </a>
+                  </h2>
+                  <p class="lead">
+                  by <a href="artsolo.php?id=
+                      <?php echo $article['id']; ?>">
+                      <?php echo $article['auteur']; ?>
+                      </a>
+                  </p>
+                  <p><span class="glyphicon glyphicon-time"></span> Posté le <?php echo $article['date'];?></p>
+                  <hr>
+                  <img class="img-responsive" src="/images/<?php echo $article['image'];?>"/>
+                  <hr>
+                  <p><?php echo $article['contenu'];?></p>
+              <hr>
+              <?php }
+              ?>
+  </div>
+</section>
+<section id="team">
+	<div class="container">
+		<div class="section-heading dark text-center">
+			<h1>Our Team</h1>
+		</div>
+		<div class="slider-wrap">
+			<div class="carousel slide" data-ride="carousel" id="quote-carousel">
+				<!-- Bottom Carousel Indicators -->
+				<ol class="carousel-indicators">
+					<li data-target="#quote-carousel" data-slide-to="0" class="active">
+						<img class="img-responsive " src="https://s3.amazonaws.com/uifaces/faces/twitter/brad_frost/128.jpg" alt="">
+					</li>
+					<li data-target="#quote-carousel" data-slide-to="1">
+						<img class="img-responsive" src="https://s3.amazonaws.com/uifaces/faces/twitter/rssems/128.jpg" alt="">
+					</li>
+					<li data-target="#quote-carousel" data-slide-to="2">
+						<img class="img-responsive" src="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg" alt="">
+					</li>
+					<li data-target="#quote-carousel" data-slide-to="3">
+						<img class="img-responsive" src="https://s3.amazonaws.com/uifaces/faces/twitter/rssems/128.jpg" alt="">
+					</li>
+					<li data-target="#quote-carousel" data-slide-to="4">
+						<img class="img-responsive" src="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg" alt="">
+					</li>
+
+
+				</ol>
+
+				<div class="carousel-inner">
+
+					<div class="item active">
+						<div class="row">
+							<div class="col-sm-2">
+								<div class="img-wrap">
+									<img src="https://s3.amazonaws.com/uifaces/faces/twitter/brad_frost/128.jpg" class="img-responsive">
+								</div>
+							</div>
+							<div class="col-sm-10">
+								<h2>Very Nice</h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. !</p>
+
+							</div>
+						</div>
+					</div>
+
+					<div class="item">
+						<div class="row">
+							<div class="col-sm-2">
+								<div class="img-wrap">
+									<img class="img-responsive" src="https://s3.amazonaws.com/uifaces/faces/twitter/rssems/128.jpg" alt="">
+								</div>
+							</div>
+							<div class="col-sm-10">
+								<h2>Kapil Verma</h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. !</p>
+
+							</div>
+						</div>
+					</div>
+					<!-- Quote 3 -->
+					<div class="item">
+						<div class="row">
+							<div class="col-sm-2">
+								<div class="img-wrap">
+									<img class="img-responsive" src="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg" alt="">
+								</div>
+							</div>
+							<div class="col-sm-10">
+								<h2>Kapil Creationz</h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. !</p>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="row">
+							<div class="col-sm-2">
+								<div class="img-wrap">
+									<img class="img-responsive" src="https://s3.amazonaws.com/uifaces/faces/twitter/rssems/128.jpg" alt="">
+								</div>
+							</div>
+							<div class="col-sm-10">
+								<h2>Kapil Verma</h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. !</p>
+
+							</div>
+						</div>
+					</div>
+					<!-- Quote 3 -->
+					<div class="item">
+						<div class="row">
+							<div class="col-sm-2">
+								<div class="img-wrap">
+									<img class="img-responsive" src="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg" alt="">
+								</div>
+							</div>
+							<div class="col-sm-10">
+								<h2>Kapil Creationz</h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. !</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<a data-slide="prev" href="#quote-carousel" class="left carousel-control"><i class="fa fa-angle-left" aria-hidden="true"></i></a>
+				<a data-slide="next" href="#quote-carousel" class="right carousel-control"><i class="fa fa-angle-right" aria-hidden="true"></i></a>
+			</div>
+		</div>
+	</div>
+</section>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
