9896a60538f9abe86784a89119f585237f2e4475
aurelie <ausaurel@gmail.com>
2018-12-29 02:22:44 +0100

    contenu textuel produits + front-end

--- produits.php
+++ produits.php
@@ -2,12 +2,15 @@
 include("header.php")
 ?>
 
-<div class="site-blocks-cover inner-page" style="background-image: url(images/hero_bg_1.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+<head>
+</head>
+<div class="site-blocks-cover inner-page" style="background-image: url(images/back_v.jpg);" data-aos="fade"
+  data-stellar-background-ratio="0.5">
   <div class="container">
     <div class="row align-items-center">
       <div class="col-md-7">
         <span class="sub-text">Présentation de nos boîtes</span>
-        <h1>Nos différents <strong>Produits</strong></h1>
+        <h1>Nos différentes <strong>Boites</strong></h1>
       </div>
     </div>
   </div>
@@ -15,15 +18,30 @@ include("header.php")
 
 <div class="site-section">
   <div class="container">
+      
+      <p class="explications" ><b>Une Box qui vous fait voyager loin… </b></p>
+      <p class="explications1"> 
+      Un peu lassés de cuisiner les mêmes plats toutes l'année ?  On a la
+      solution  pour vous : <b>un concept de voyage culinaire, ludique et instructif.</b> Nous travaillons avec la présence
+      incontestée d’un Grand Chef étoilé qui nous permet de proposé des livres de recettes riche en <b>expériences et en
+      savoir-faires.</b>
+      
+      
+    </p>
+    <h2 class="explications2"> Toute les boites contiennent <b>un livre de recette et des anecdotes !</b> </h2>
+
     <div class="row">
-      <div class="col-md-6 bg-image bg-sm-height mb-5 mb-md-0 order-md-2" style="background-image: url('images/img_1_colored.jpg');" data-aos="fade-up"></div>
+
+      <div class="col-md-6 bg-image bg-sm-height mb-5 mb-md-0 order-md-2" style="background-image: url('images/boite.png');"
+        data-aos="fade-up"></div>
+
       <div class="col-md-6 pr-md-5 order-md-1">
-        <h2 class="display-3 line-height-xs text-black mb-4">La boite <strong>économique</strong></h2>
-        <p class="mb-4">Magnam iure fugit recusandae nobis a amet, officiis laboriosam repudiandae? Quis nostrum numquam ducimus quo ab laboriosam qui expedita, cupiditate ex, sed dignissimos facere provident dolores, eius distinctio quas aliquid.</p>
+        <h2 class="display-3m ">La boite <strong>économique</strong></h2>
+
         <ul class="site-block-check">
-          <li>Magnam iure fugit recusandae</li>
-          <li>Officiis laboriosam repudiandae</li>
-          <li>Quis nostrum numquam</li>
+          <li> <img src="images/c2.png" alt="liste round" height="25" width="25">  3-4 ingrédients choisis par notre chef étoilé.</li>
+          <li><img src="images/c2.png" alt="liste round" height="25" width="25">  Cuisinez facilement à petit prix.</li>
+          <li><img src="images/c2.png" alt="liste round" height="25" width="25"> Envie d’un repas à deux ? </li>
         </ul>
         <p><a href="#" class="btn btn-outline-primary btn-sm rounded-0 p-2 px-4">En savoir plus</a></p>
       </div>
@@ -34,14 +52,15 @@ include("header.php")
 <div class="site-section">
   <div class="container">
     <div class="row">
-      <div class="col-md-6 bg-image bg-sm-height mb-5 mb-md-0 order-md-1" style="background-image: url('images/img_1_colored.jpg');" data-aos="fade-up"></div>
+      <div class="col-md-6 bg-image bg-sm-height mb-5 mb-md-0 order-md-1" style="background-image: url('images/boite2.png');"
+        data-aos="fade-up"></div>
       <div class="col-md-6 pr-md-5 order-md-2">
-        <h2 class="display-3 line-height-xs text-black mb-4">La boîte <strong>classique</strong></h2>
-        <p class="mb-4">Magnam iure fugit recusandae nobis a amet, officiis laboriosam repudiandae? Quis nostrum numquam ducimus quo ab laboriosam qui expedita, cupiditate ex, sed dignissimos facere provident dolores, eius distinctio quas aliquid.</p>
+        <h2 class="display-3m ">La boîte <strong>classique</strong></h2>
+
         <ul class="site-block-check">
-          <li>Magnam iure fugit recusandae</li>
-          <li>Officiis laboriosam repudiandae</li>
-          <li>Quis nostrum numquam</li>
+          <li> <img src="images/c2.png" alt="liste round" height="25" width="25"> 6-7 Ingrédients choisis par notre chef étoilé.</li>
+          <li> <img src="images/c2.png" alt="liste round" height="25" width="25"> Recommandé par notre équipe de professionnels.</li>
+          <li> <img src="images/c2.png" alt="liste round" height="25" width="25"> Pour des repas en famille ou entre amis. </li>
         </ul>
         <p><a href="#" class="btn btn-outline-primary btn-sm rounded-0 p-2 px-4">En savoir plus</a></p>
       </div>
@@ -52,14 +71,15 @@ include("header.php")
 <div class="site-section">
   <div class="container">
     <div class="row">
-      <div class="col-md-6 bg-image bg-sm-height mb-5 mb-md-0 order-md-2" style="background-image: url('images/img_1_colored.jpg');" data-aos="fade-up"></div>
+      <div class="col-md-6 bg-image bg-sm-height mb-5 mb-md-0 order-md-2" style="background-image: url('images/boite3.png');"
+        data-aos="fade-up"></div>
       <div class="col-md-6 pr-md-5 order-md-1">
-        <h2 class="display-3 line-height-xs text-black mb-4">La boîte <br><strong>sur mesure</strong></h2>
-        <p class="mb-4">Magnam iure fugit recusandae nobis a amet, officiis laboriosam repudiandae? Quis nostrum numquam ducimus quo ab laboriosam qui expedita, cupiditate ex, sed dignissimos facere provident dolores, eius distinctio quas aliquid.</p>
+        <h2 class="display-3m ">La boîte <br><strong>sur mesure</strong></h2>
+
         <ul class="site-block-check">
-          <li>Magnam iure fugit recusandae</li>
-          <li>Officiis laboriosam repudiandae</li>
-          <li>Quis nostrum numquam</li>
+        <li> <img src="images/c2.png" alt="liste round" height="25" width="25"> 6-7 Ingrédients choisis par notre chef étoilé.</li>
+        <li> <img src="images/c2.png" alt="liste round" height="25" width="25"> A vous de choisir votre pays !</li>
+         <li> <img src="images/c2.png" alt="liste round" height="25" width="25"> Une idée originale de cadeau à offrir.</li>
         </ul>
         <p><a href="#" class="btn btn-outline-primary btn-sm rounded-0 p-2 px-4">En savoir plus</a></p>
       </div>
@@ -67,89 +87,6 @@ include("header.php")
   </div>
 </div>
 
-<div class="site-section">
-  <div class="container">
-    <div class="row mb-5 justify-content-center">
-      <div class="col-md-6 text-center">
-        <h2 class="display-4 text-black mb-5">See Our <strong>Services</strong></h2>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-6 col-lg-4">
-        <div class="site-block-feature-2 d-flex rounded mb-5">
-          <div class="mr-3">
-            <span class="icon flaticon-agreement font-weight-light "></span>
-          </div>
-          <div class="text">
-            <h3>Sunt illum fugit doloremque</h3>
-            <p>Sunt illum fugit doloremque eaque omnis dolor sint fuga eligendi quis magnam Laboriosam facilis.</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md-6 col-lg-4">
-        <div class="site-block-feature-2 d-flex rounded mb-5">
-          <div class="mr-3">
-            <span class="icon flaticon-gears font-weight-light "></span>
-          </div>
-          <div class="text">
-            <h3>Sunt illum fugit doloremque</h3>
-            <p>Sunt illum fugit doloremque eaque omnis dolor sint fuga eligendi quis magnam Laboriosam facilis.</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md-6 col-lg-4">
-        <div class="site-block-feature-2 d-flex rounded mb-5">
-          <div class="mr-3">
-            <span class="icon flaticon-excavator font-weight-light "></span>
-          </div>
-          <div class="text">
-            <h3>Sunt illum fugit doloremque</h3>
-            <p>Sunt illum fugit doloremque eaque omnis dolor sint fuga eligendi quis magnam Laboriosam facilis.</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md-6 col-lg-4">
-        <div class="site-block-feature-2 d-flex rounded mb-5">
-          <div class="mr-3">
-            <span class="icon flaticon-industrial-robot font-weight-light "></span>
-          </div>
-          <div class="text">
-            <h3>Sunt illum fugit doloremque</h3>
-            <p>Sunt illum fugit doloremque eaque omnis dolor sint fuga eligendi quis magnam Laboriosam facilis.</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md-6 col-lg-4">
-        <div class="site-block-feature-2 d-flex rounded mb-5 ng">
-          <div class="mr-3">
-            <span class="icon flaticon-industry font-weight-light "></span>
-          </div>
-          <div class="text">
-            <h3>Sunt illum fugit doloremque</h3>
-            <p>Sunt illum fugit doloremque eaque omnis dolor sint fuga eligendi quis magnam Laboriosam facilis.</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md-6 col-lg-4">
-        <div class="site-block-feature-2 d-flex rounded mb-5">
-          <div class="mr-3">
-            <span class="icon flaticon-worker font-weight-light "></span>
-          </div>
-          <div class="text">
-            <h3>Sunt illum fugit doloremque</h3>
-            <p>Sunt illum fugit doloremque eaque omnis dolor sint fuga eligendi quis magnam Laboriosam facilis.</p>
-          </div>
-        </div>
-      </div>
-
-    </div>
-  </div>
-</div>
 
 <?php
 include("footer.php");
